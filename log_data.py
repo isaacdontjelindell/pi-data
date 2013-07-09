04d99fd5df3647239b80d56394acc621300eda04
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-09 13:07:14 -0500

    allow easy quitting

--- log_data.py
+++ log_data.py
@@ -2,6 +2,7 @@ import threading
 import time
 import Queue
 import random
+import sys
 #import serial
 
 
@@ -11,46 +12,59 @@ import random
 #response = serialport.readlines(None)
 #print response
 
-def simSerialPort(input_queue):
+def sim_collectData(input_queue, stop_event):
     ''' this provides some output simulating the serial
     data from the data logging hardware. 
     '''
     n = 0
-    # while n < 10
-    while True:
-        input_queue.put("DATA: <here are some random data>")
-        time.sleep(random.randint(0,10))
+    while not stop_event.is_set():
+        input_queue.put("DATA: <here are some random data> " + str(n))
+        stop_event.wait(0.001)
         n += 1
+    input_queue.put(None) # send a signal telling the logging thread we're done
+    print "[collection thread] Terminated data collection."
+    return
 
 
-def loggingThread(input_queue):
+def logData(input_queue):
     n = 0
 
-    # this finishes after 10 lines are recieved. This is to allow
-    # the program to terminate. In production, will probably want to 
-    # use 
+    # if the stop event is recieved and the previous loop terminates, 
+    # finish logging the rest of the items in the queue.
     while True:
-    #while n < 10:
         d = input_queue.get()
+        if d is None:
+            input_queue.task_done()
+            print "[logging thread] Finished logging."
+            return
         if d.startswith("DATA:"):
-            print d
-        input_queue.task_done()
-        n += 1
-    return
+            print "[logging thread] Logged to DB: " + d
+            input_queue.task_done()
+            n += 1
 
 
 def main():
     input_queue = Queue.Queue()
     
-    print "Starting data collection thread...",
-    collection_thread = threading.Thread(target=simSerialPort, args=(input_queue,))
+    stop_event = threading.Event() # used to signal termination to the thread
+    
+    print "[main] Starting data collection thread...",
+    collection_thread = threading.Thread(target=sim_collectData, args=(input_queue, stop_event))
     collection_thread.start()
-    print("Done.")
+    print "Done."
 
-    print "Starting logging thread...",
-    logging_thread = threading.Thread(target=loggingThread, args=(input_queue,))
+    print "[main] Starting logging thread...",
+    logging_thread = threading.Thread(target=logData, args=(input_queue, ))
     logging_thread.start()
-    print("Done.")
+    print "Done."
 
+    try:
+        while True:
+            time.sleep(10)
+    except (KeyboardInterrupt, SystemExit):
+        # stop data collection. Let the logging thread finish logging everything in the queue
+        stop_event.set()
+        collection_thread.join()
+        logging_thread.join()
 
 main()
