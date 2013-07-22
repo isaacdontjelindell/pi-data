080a72704330c348937b5d6dcfb408d87e89087e
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-07-21 20:50:36 -0500

    add data upload task

--- README.md
+++ README.md
@@ -6,8 +6,20 @@ Data logging project using a serial interface to the Raspberry Pi
 db schema
 ---------
 CREATE TABLE example (
+    id MEDIUMINT NOT NULL AUTO_INCREMENT,
     data VARCHAR(100),
     remotedatetime DATETIME,
-    uploaded TINYINT(1)
+    uploaded TINYINT(1),
+    PRIMARY KEY (id)
 );
 
+
+dependencies
+------------
+mysqldb
+requests
+
+
+TODO
+----
+Time format? (UTC, tz, etc)
---
+++ upload_data.py
@@ -0,0 +1,37 @@
+import MySQLdb as mdb
+import sys
+import requests
+
+USERNAME = 'testuser'
+PW = 'summerD82'
+HOST = 'http://lumafile.com/uploaddev/'
+
+def main():
+    try:
+        con = mdb.connect('localhost', 'edatauser', 'wibble23wobble', 'edata')
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0],e.args[1])
+        sys.exit(1)
+
+    sql_query = '''SELECT * FROM example WHERE uploaded = 0 AND id=2'''
+    with con:
+        cur = con.cursor(mdb.cursors.DictCursor)
+        cur.execute(sql_query)
+        records = cur.fetchall()
+
+    for record in records:
+        payload = {'username':USERNAME,
+                   'password':PW,
+                   'data':record['data'],
+                   'remotedatetime':record['remotedatetime']}
+        response = requests.post(HOST, data=payload)
+        print response.status_code
+
+        if response.status_code == 200:
+            sql = ''' UPDATE example SET uploaded = 1 WHERE id = %s ''' % record['id']
+            with con:
+                cur = con.cursor(mdb.cursors.DictCursor)
+                cur.execute(sql)
+    con.close()
+
+main()
